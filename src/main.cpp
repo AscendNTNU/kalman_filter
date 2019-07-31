e0080d5cb3b7756495aef19335bb2e994b5afabf
Nadia Wangberg <nadiawangberg98@gmail.com>
2019-07-31 06:28:34 +0200

    Send pose through with covariance

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,14 @@
 #include <iostream>
 #include <tf/tf.h>
 #include <Eigen/Geometry>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
+ros::Publisher pub; 
+void republish(const geometry_msgs::PoseWithCovarianceStamped& input){
+	pub.publish(input);
+}
+
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "kalman");
@@ -16,11 +24,14 @@ int main(int argc, char** argv) {
 
     filter.printSystem(); 
 
-    ros::Subscriber sub_pose = n.subscribe("zed/zed_node/pose", 1, &Kalman::correction, &filter); 
-    while(ros::ok()){
-        filter.prediction(); 
-        ros::spinOnce();
-        filter.publish(); 
-        rate.sleep();
-    }
+    ros::Subscriber sub_pose = n.subscribe("zed/zed_node/pose_with_covariance", 1, republish); 
+	pub = n.advertise<geometry_msgs::PoseWithCovarianceStamped>("mavros/vision_pose/pose_cov", 1);
+	ros::spin();
+
+    //while(ros::ok()){
+    //    filter.prediction(); 
+    //    ros::spinOnce();
+    //    filter.publish(); 
+    //    rate.sleep();
+    //}
 }
